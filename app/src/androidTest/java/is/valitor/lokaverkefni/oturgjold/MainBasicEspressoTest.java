1d4d48561646be201a0a453d164d4797722450ef
dasverrisson <dasverrisson@gmail.com>
2015-02-12 13:22:23 +0000

    Two espresso UI tests, giving ID´s to register_card layout elements.

--- app/src/androidTest/java/is/valitor/lokaverkefni/oturgjold/MainBasicEspressoTest.java
+++ app/src/androidTest/java/is/valitor/lokaverkefni/oturgjold/MainBasicEspressoTest.java
@@ -1,15 +1,18 @@
 package is.valitor.lokaverkefni.oturgjold;
 
 import android.app.Application;
+import android.support.test.espresso.action.ViewActions;
 import android.test.ActivityInstrumentationTestCase2;
 import android.test.ApplicationTestCase;
 import android.test.suitebuilder.annotation.LargeTest;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isClickable;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static android.support.test.espresso.action.ViewActions.*;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
@@ -29,6 +32,24 @@ public class MainBasicEspressoTest extends ActivityInstrumentationTestCase2<Main
 
     public void testButtonExists() {
         onView(withId(R.id.button_register_card)).check(matches(isDisplayed()));
+    }
+
+    public void testClickClicks() {
+        onView(withId(R.id.button_register_card)).perform(click());
+        onView(withId(R.id.button_register_card_next)).check(matches(isDisplayed()));
+
+    }
+
+    public void testTextEdits() {
+        onView(withId(R.id.button_register_card)).perform(click());
+
+        onView(withId(R.id.editCardholderName)).perform(typeText("Micheal Jordan"));
+        onView(withId(R.id.editCardNumber)).perform(typeText("4539481729817506"));
+        onView(withId(R.id.editCardCvv)).perform(typeText("666"));
+        onView(withId(R.id.editCardCvv)).perform(ViewActions.closeSoftKeyboard());
+        onView(withId(R.id.button_register_card_next)).check(matches(isClickable()));
+        onView(withId(R.id.button_register_card_next)).perform(ViewActions.doubleClick());
+        onView(withId(R.id.tokenize_linear)).check(matches(isDisplayed()));
     }
 
 }
