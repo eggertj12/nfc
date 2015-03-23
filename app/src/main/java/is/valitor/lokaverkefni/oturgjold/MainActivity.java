b4e7ca2443e91ebe2623403bcf4b52a65d7e369c
Kristrun <kristrun12@ru.is>
2015-03-23 21:57:20 +0000

    register card in settings

--- app/src/main/java/is/valitor/lokaverkefni/oturgjold/MainActivity.java
+++ app/src/main/java/is/valitor/lokaverkefni/oturgjold/MainActivity.java
@@ -54,7 +54,8 @@ public class MainActivity extends Activity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.register_card) {
+            registerCard(item.getActionView());
             return true;
         }
 
